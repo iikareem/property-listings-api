fa5ce9c4f80db517cdaa1b044bc4cce72164d962
Kareem Ashraf <77942683+iikareem@users.noreply.github.com>
2026-05-01 15:06:59 +0300

    chore(docker): simplify entrypoint since migration/seed are separate services

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations..."
-node_modules/.bin/ts-node --transpile-only -r tsconfig-paths/register node_modules/typeorm/cli.js migration:run -d data-source.ts
-
-echo "Seeding database..."
-node_modules/.bin/ts-node --transpile-only -r tsconfig-paths/register seeds/seeder.ts
-
 echo "Starting application..."
 exec node dist/src/main.js
