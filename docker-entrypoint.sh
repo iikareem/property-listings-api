663bf44e26acdbb7367abbad558d9dbcb461ab03
Kareem Ashraf <77942683+iikareem@users.noreply.github.com>
2026-05-01 10:31:19 +0300

    feat(docker): add entrypoint script for migrations, seeding, and app startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
 set -e
 
-echo "Running migrations"
+echo "Running migrations..."
 node_modules/.bin/ts-node --transpile-only -r tsconfig-paths/register node_modules/typeorm/cli.js migration:run -d data-source.ts
 
-echo "Seeding database"
-node_modules/.bin/ts-node --transpile-only -r tsconfig-paths/register src/seeder.ts
+echo "Seeding database..."
+node_modules/.bin/ts-node --transpile-only -r tsconfig-paths/register seeds/seeder.ts
 
-echo "Starting app..."
-node dist/main
+echo "Starting application..."
+exec node dist/src/main.js
