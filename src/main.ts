856c0b94db1ec9d0445545b72733e14bf9661c80
Kareem Ashraf <77942683+iikareem@users.noreply.github.com>
2026-05-01 15:57:45 +0300

    chore(app): log port number on startup

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap().catch(console.error);
