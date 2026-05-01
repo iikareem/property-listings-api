accbef8a0f058d8c13878e2c0df9625cbd985d5c
Kareem Ashraf <77942683+iikareem@users.noreply.github.com>
2026-05-01 12:41:24 +0300

    refactor(property): import RedisModule for cache dependency injection

--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Property } from './entities/property.entity';
 import { PropertyService } from './property.service';
 import { PropertyController } from './property.controller';
+import { RedisModule } from '../cache/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Property])],
+  imports: [TypeOrmModule.forFeature([Property]), RedisModule],
   controllers: [PropertyController],
   providers: [PropertyService],
 })
